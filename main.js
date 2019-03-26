a03640fe4cbb26c180ab0419919151d2a1dba93a
BryantBuilt <bryantp24@gmail.com>
2019-03-26 16:56:36 -0700

    second commit

--- main.js
+++ main.js
@@ -3,4 +3,13 @@ $(document).ready(function() {
     fullWidth: true,
     indicators: true
   });
+
+  $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+
+  $('.toggle-newsletter-modal').click(function(){
+      $('.newsletter-modal').toggleClass('is-active')
+  });
 });
