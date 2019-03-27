c99e0d83247417eb97a7ed92843a6b4d61e19628
BryantBuilt <bryantp24@gmail.com>
2019-03-26 22:21:30 -0700

    added navbar and modal

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
+
   $(".carousel.carousel-slider").carousel({
     fullWidth: true,
     indicators: true
   });
+
+  $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+
+  $('.toggle-newsletter-modal').click(function(){
+    $('.newsletter-modal').toggleClass('is-active')
+  });
+
 });
